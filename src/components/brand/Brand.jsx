4ec6481fa1f67944a3fac8a5b716659c595c0549
venya251 <123720637+venya251@users.noreply.github.com>
2024-02-26 09:58:50 -0600

    Update Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -3,9 +3,23 @@ import './brand.css'
 
 const Brand = () => {
   return (
+    <div className="gpt3__brand section__padding">
     <div>
-      Brand
+      <img src={google} />
     </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
   )
 }
 
